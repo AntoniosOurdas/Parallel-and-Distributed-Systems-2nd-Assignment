1eeec357a1b7633ee55808fbcc68433377f322e8
AntoniosOurdas <45215986+Tony31614@users.noreply.github.com>
2020-12-22 22:06:35 +0200

    Updated Makefile and scripts

--- src/script.sh
+++ src/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make clean
-make all	
-./V0 10 10 "$1"	
+make all
+./V0 "$1" "$2" "$3" "$4"	
--- src/scriptDocker.sh
+++ src/scriptDocker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pwd
 make clean
 make all
-Docker-main/pds-codebase/2nd_Assignment/V0 "$1" "$2" "$3" "$4"	
+V0 "$1" "$2" "$3" "$4"
